9c1f8f22d67cc152373eec45e6ee0dd4ecc10f93
Margarida <margarida97pt@gmail.com>
2021-07-09 17:09:21 +0100

    Update

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../App.css';
+import Mockup from './Mockup';
+
+function App() {
+  return (
+    <div>
+      <Mockup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mockup.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../components/Mockup.css";
+
+const Mockup = () => {
+  return (
+   <>
+   <h1>Clean Kangaroo Prototype</h1>
+    <video width="80%" controls autoplay className="video">
+        <source src="Margarida Dias - Clean Kangaroo.mp4" type="video/mp4"/>
+    </video>
+   </>
+  );
+};
+
+export default Mockup;
