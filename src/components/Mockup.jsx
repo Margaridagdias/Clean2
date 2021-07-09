d1baf1edff3b7a9b25e88c3d8cb9466e5da8b4e8
Margarida <margarida97pt@gmail.com>
2021-07-09 20:06:00 +0100

    Update

--- src/components/Mockup.jsx
+++ src/components/Mockup.jsx
@@ -3,12 +3,17 @@ import "../components/Mockup.css";
 
 const Mockup = () => {
   return (
-   <>
-   <h1>Clean Kangaroo Prototype</h1>
-    <video width="80%" controls autoplay className="video">
-        <source src="Margarida Dias - Clean Kangaroo.mp4" type="video/mp4"/>
-    </video>
-   </>
+    <>
+      <h1>Clean Kangaroo Prototype</h1>
+      <video width="80%" controls autoplay className="video">
+        <source src="Margarida Dias - Clean Kangaroo.mp4" type="video/mp4" />
+      </video>
+      <h4 className="details">
+        More details on&nbsp;<a href="https://www.behance.net/gallery/123171895/Clean-Kangaroo" className="Link">
+          Behance
+        </a>
+      </h4>
+    </>
   );
 };
 
